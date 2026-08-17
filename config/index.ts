5c2e58f364cbdb30cce880e1b6513142f4885ab5
bintangingaming <bintangingaming@gmail.com>
2026-08-17 16:23:31 +0700

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,9 @@
 import type { AppInfo } from '@/types/app'
+
 export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
+
 export const APP_INFO: AppInfo = {
   title: 'LUMI',
   description: 'LUMI (*Learning, Understanding, Mental Insight*) adalah asisten belajar AI interaktif yang dirancang khusus untuk mendampingi siswa SMA. Menggunakan pendekatan metode Socrates. LUMI membimbing kamu memecahkan soal matematika, sains, dan pelajaran lainnya lewat pertanyaan pemancing yang melatih pola pikir kritis.',
@@ -18,4 +20,4 @@ export const API_PREFIX = '/api'
 
 export const LOCALE_COOKIE_NAME = 'locale'
 
-export const DEFAULT_VALUE_MAX_LEN = 48
+export const DEFAULT_VALUE_MAX_ LEN = 48
